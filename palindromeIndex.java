70201bbf88ad279ad94ef524a45e0f8856dd9aa7
hema3rafa <hema3rafa@gmail.com>
2024-03-09 13:57:38 +0200

    Create palindromeIndex.java

---
+++ palindromeIndex.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'palindromeIndex' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int palindromeIndex(String s) {
+    // Write your code here
+     String[] arr = s.split("");
+    for (int i = 0; i < arr.length; i++) {
+            if(!arr[i].equals(arr[arr.length -i -1]))
+            {
+        
+                if(i < arr.length && arr[i+1].equals(arr[arr.length -i-1]) && arr[i+2].equals(arr[arr.length -i-2]))
+                {
+                    return i ;
+                }else if(arr[i].equals(arr[arr.length -i-2]))
+                {
+                   return arr.length -i-1 ;
+ 
+                }
+            
+            }
+
+        
+    }
+    
+    return -1;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.palindromeIndex(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
